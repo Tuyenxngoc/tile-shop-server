582131d64d78e1a19eab80cb32ab75a6d00a9528
tuyenxngoc <tuyenn9448@gmail.com>
2025-03-17 22:36:25 +0700

    first commit

---
+++ src/main/java/com/example/tileshop/TileshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.tileshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TileshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TileshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tileshop/TileshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tileshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TileshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
