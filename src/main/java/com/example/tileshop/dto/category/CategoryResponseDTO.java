16190cbef24a295b84885fd8893c0aec22d0c62a
tuyenxngoc <tuyenn9448@gmail.com>
2025-03-25 17:29:29 +0700

    feat: update CategoryResponseDTO to use BaseEntityDTO for parent field

--- src/main/java/com/example/tileshop/dto/category/CategoryResponseDTO.java
+++ src/main/java/com/example/tileshop/dto/category/CategoryResponseDTO.java
@@ -1,5 +1,6 @@
 package com.example.tileshop.dto.category;
 
+import com.example.tileshop.dto.common.BaseEntityDTO;
 import com.example.tileshop.entity.Category;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,14 +10,14 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 public class CategoryResponseDTO {
-    private Long id;
+    private long id;
     private String name;
-    private Long parentId;
+    private BaseEntityDTO parent;
 
     public CategoryResponseDTO(Category category) {
         this.id = category.getId();
         this.name = category.getName();
-        this.parentId = category.getParent() != null ? category.getParent().getId() : null;
+        this.parent = category.getParent() != null ? new BaseEntityDTO(category.getParent().getId(), category.getParent().getName()) : null;
     }
 
 }
