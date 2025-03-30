61a8fa0ac4e6adb3378b59edf68eb0711fd9da62
tuyenxngoc <tuyenn9448@gmail.com>
2025-03-30 22:33:25 +0700

    refactor: update Product dto

--- src/main/java/com/example/tileshop/dto/product/ProductRequestDTO.java
+++ src/main/java/com/example/tileshop/dto/product/ProductRequestDTO.java
@@ -1,30 +1,39 @@
 package com.example.tileshop.dto.product;
 
+import com.example.tileshop.constant.ErrorMessage;
 import com.example.tileshop.dto.productattribute.ProductAttributeRequestDTO;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
 public class ProductRequestDTO {
+    @NotBlank(message = ErrorMessage.INVALID_NOT_BLANK_FIELD)
+    @Size(min = 3, max = 255, message = ErrorMessage.INVALID_TEXT_LENGTH)
     private String name;
 
     private String description;
 
+    @NotNull(message = ErrorMessage.INVALID_SOME_THING_FIELD_IS_REQUIRED)
     private Double price;
 
+    @NotNull(message = ErrorMessage.INVALID_SOME_THING_FIELD_IS_REQUIRED)
+    @Min(value = 0, message = ErrorMessage.INVALID_MINIMUM_ONE)
+    @Max(value = 100, message = ErrorMessage.INVALID_MAXIMUM_ONE_HUNDRED)
     private Double discountPercentage;
 
+    @NotNull(message = ErrorMessage.INVALID_SOME_THING_FIELD_IS_REQUIRED)
     private Integer stockQuantity;
 
-    private Double averageRating;
-
+    @NotNull(message = ErrorMessage.INVALID_SOME_THING_FIELD_IS_REQUIRED)
     private Long categoryId;
 
     private Long brandId;
 
-    private List<ProductAttributeRequestDTO> attributes = new ArrayList<>();
+    @NotNull(message = ErrorMessage.INVALID_SOME_THING_FIELD_IS_REQUIRED)
+    private List<@NotNull(message = ErrorMessage.INVALID_SOME_THING_FIELD_IS_REQUIRED)
+            ProductAttributeRequestDTO> attributes;
 }
--- src/main/java/com/example/tileshop/dto/productattribute/ProductAttributeRequestDTO.java
+++ src/main/java/com/example/tileshop/dto/productattribute/ProductAttributeRequestDTO.java
@@ -1,18 +1,22 @@
 package com.example.tileshop.dto.productattribute;
 
-import com.example.tileshop.entity.Attribute;
-import com.example.tileshop.entity.Product;
+import com.example.tileshop.constant.ErrorMessage;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class ProductAttributeRequestDTO {
-
+    @NotBlank(message = ErrorMessage.INVALID_NOT_BLANK_FIELD)
+    @Size(max = 255, message = ErrorMessage.INVALID_TEXT_LENGTH)
     private String value;
 
-    private Product productId;
+    @NotNull(message = ErrorMessage.INVALID_SOME_THING_FIELD_IS_REQUIRED)
+    private Long productId;
 
-    private Attribute attributeId;
-
+    @NotNull(message = ErrorMessage.INVALID_SOME_THING_FIELD_IS_REQUIRED)
+    private Long attributeId;
 }
